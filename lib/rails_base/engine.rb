81d8e2fb5bf95edb40c61804f4a73489cd1c4ee5
Quo yi <beitaz@icloud.com>
2020-08-14 11:47:43 +0800

    update generator config

--- lib/rails_base/engine.rb
+++ lib/rails_base/engine.rb
@@ -1,5 +1,20 @@
 module RailsBase
   class Engine < ::Rails::Engine
     isolate_namespace RailsBase
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
+    config.generators do |g|
+      g.assets false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
+
+      # g.templates.prepend File.expand_path('lib/templates', RailsBase::Engine.root)
+    end
   end
 end
